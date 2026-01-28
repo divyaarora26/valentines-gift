22b7bdf7e493594660a4066c2410f580042b1f1a
divyaarora26 <divyaarora6543@gmail.com>
2026-01-28 14:13:24 +0530

    Add function to transition to phase 2

---
+++ script.js
@@ -0,0 +1,5 @@
+function goToPhase2() {
+  document.getElementById("phase1").style.display = "none";
+  document.getElementById("phase2").style.display = "block";
+  document.getElementById("music").play();
+}
